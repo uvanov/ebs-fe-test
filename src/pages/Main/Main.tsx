50626439dfbd7272edb9365c9135e1a43e47d84a
uvanov <kirill.ivanov000@gmail.com>
2022-07-18 12:43:57 +0300

    Main page layout

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Grid } from '@mui/material';
+
+import Filter from '../../components/layout/Filter/Filter';
+import Products from '../../components/layout/Products/Products';
 
 const Main: React.FC = () => {
+
   return (
-    <div>
-      Main
-    </div>
+    <Grid
+      container
+      spacing={ 2 }
+      sx={{ padding: '20px' }}
+    >
+      <Grid item xs={ 2 }>
+        <Filter />
+      </Grid>
+      <Grid item xs={ 10 }>
+        <Products />
+      </Grid>
+    </Grid>
   );
 };
 
