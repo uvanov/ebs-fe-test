7a20fedde1c48b61329420759d945eaece701149
uvanov <kirill.ivanov000@gmail.com>
2022-07-18 12:44:29 +0300

    Show text if cart is empty

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Grid, Typography } from '@mui/material';
+import { ProductContext } from '../../contexts/ProductContext/ProductContext';
+
+import ProductCard from '../../components/layout/ProductCard/ProductCard';
 
 const Cart: React.FC = () => {
+
+  const { state } = useContext(ProductContext);
+
   return (
-    <div>
-      Cart
-    </div>
+    <Grid
+      container
+      spacing={ 2 }
+      sx={ { padding: '20px 50px' } }
+    >
+      {
+        state.cartProducts.length >= 1
+          ? state.cartProducts.map(product => (
+            <Grid item xs={ 3 }>
+              <ProductCard product={ product } />
+            </Grid>
+          ))
+          : <Typography>Empty cart</Typography>
+      }
+    </Grid>
   );
 };
 
